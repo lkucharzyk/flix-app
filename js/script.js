2df30b38191895aa98d10950341692307d1e6199
lkucharzyk <lukaszkucharzyk17@gmail.com>
2023-05-08 09:30:11 +0200

    added loading spinner and display tv shows

--- js/script.js
+++ js/script.js
@@ -17,12 +17,14 @@ function highlightActiveLink(){
 }
 
 async function fetchAPIData(endpoint){
-    const APIKey = 'placeholder';
+    const APIKey = '9bdba24599e2e9c77fcd3fd09d83c7eb';
     const APIURL = 'https://api.themoviedb.org/3/';
+    showSpinner();
     
     const res = await fetch(`${APIURL}${endpoint}?api_key=${APIKey}&language=pl-PL`);
     const data = await res.json();
 
+    hideSpinner();
     return data;
 }
 
@@ -43,7 +45,7 @@ async function displayPopularMovies(){
 
           ` src="images/no-image.jpg"
               class="card-img-top"
-              alt="Movie Title"`
+              alt="${movie.name}"`
         }
         />
       </a>
@@ -57,6 +59,45 @@ async function displayPopularMovies(){
     })
 }
 
+async function displayPopularShows(){
+    const { results } = await fetchAPIData('tv/popular');
+    console.log(results);
+    results.forEach(movie =>{
+        const movieCard = document.createElement('div');
+        movieCard.classList.add('card');
+        movieCard.innerHTML = `<a href="tv-details.html?id=${movie.id}">
+        <img
+        ${
+            movie.poster_path ? 
+         `src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+          class="card-img-top"
+          alt="${movie.name}"`:
+
+          ` src="images/no-image.jpg"
+              class="card-img-top"
+              alt="${movie.name}"`
+        }
+        />
+      </a>
+      <div class="card-body">
+        <h5 class="card-title">${movie.name}</h5>
+        <p class="card-text">
+          <small class="text-muted">Air date: ${movie.first_air_date}</small>
+        </p>
+      </div>`
+      document.querySelector('#popular-shows').appendChild(movieCard);
+    })
+}
+
+function showSpinner(){
+    const spinnner = document.querySelector('.spinner');
+    spinnner.classList.add('show');
+}
+
+function hideSpinner(){
+    const spinnner = document.querySelector('.spinner');
+    spinnner.classList.remove('show');
+}
 
 function init(){
     switch (state.currentPage){
@@ -65,7 +106,7 @@ function init(){
             displayPopularMovies();
         break;
         case '/shows.html':
-
+            displayPopularShows();
         break;
         case '/movie-details.html':
 
